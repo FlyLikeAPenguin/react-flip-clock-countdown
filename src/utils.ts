7da8dfb383e94a62f980074d1328a922d7781a15
Joe Thornton <joe.clifford.thornton@gmail.com>
2024-02-11 10:45:02 +0900

    fix: attempt to handle counter getting stuck at 0

--- src/utils.ts
+++ src/utils.ts
@@ -15,14 +15,23 @@ export function calcTimeDelta(target: Date | number | string): FlipClockCountdow
   }
   const now = new Date();
   const timeLeft = Math.round((date.getTime() - now.getTime()) / 1000); // convert to seconds
-
-  return {
-    total: timeLeft,
-    days: Math.floor(Math.abs(timeLeft / (24 * 60 * 60))),
-    hours: Math.floor(Math.abs((timeLeft / 3600) % 24)),
-    minutes: Math.floor(Math.abs((timeLeft / 60) % 60)),
-    seconds: Math.floor(Math.abs(timeLeft % 60))
-  };
+  if (timeLeft > 0) {
+    return {
+      total: timeLeft,
+      days: Math.floor(Math.abs(timeLeft / (24 * 60 * 60))),
+      hours: Math.floor(Math.abs((timeLeft / 3600) % 24)),
+      minutes: Math.floor(Math.abs((timeLeft / 60) % 60)),
+      seconds: Math.floor(Math.abs(timeLeft % 60))
+    };
+  } else {
+    return {
+      total: timeLeft,
+      days: Math.ceil(Math.abs(timeLeft / (24 * 60 * 60))),
+      hours: Math.ceil(Math.abs((timeLeft / 3600) % 24)),
+      minutes: Math.ceil(Math.abs((timeLeft / 60) % 60)),
+      seconds: Math.ceil(Math.abs(timeLeft % 60))
+    };
+  }
 }
 
 export function pad(n: number): Digit[] {
